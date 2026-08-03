720ea6cc1d766e7ba89b1e8f7866fc6c6623632d
Asersaai <132582571+Asersaai@users.noreply.github.com>
2026-08-04 00:39:22 +0500

    first commit

---
+++ taskflow-b/src/main/java/com/asersaai/taskflowb/TaskflowBApplication.java
@@ -0,0 +1,13 @@
+package com.asersaai.taskflowb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskflowBApplication.class, args);
+    }
+
+}
